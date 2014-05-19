97e788864a47ce633b669be0d9e1f0d424ab3166
Jon <jbent14@smpanthers.org>
2014-05-19 13:54:14 -0700

    fixed collatz

--- collatz.js
+++ collatz.js
@@ -1,2 +1,17 @@
-console.log("Jon's Collatz")
 
+var collatz = function(n) {
+    console.log(n);
+    while (n > 1) {
+        if (n % 2 === 0) {
+            n = (n / 2);
+        }
+        else {
+            n = (3 * n + 1);
+        }
+       
+
+    console.log(n);
+    }
+};
+
+return collatz(59);
